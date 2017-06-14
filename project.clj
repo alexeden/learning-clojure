468ef44ce911ddf5c49b49a35195b10293fa880c
alexeden <AlexanderEden91@gmail.com>
2017-06-13 20:33:20 -0500

    Updating clojure dep version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/alexeden/learning-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
   :main ^:skip-aot learning-rxjs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
